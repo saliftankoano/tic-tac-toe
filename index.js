4b5638a128b6c952e6d19f8f60c7edb3f4e5e97b
Salif Tankoano <tanksalif@gmail.com>
2023-10-01 15:04:11 -0400

    add winner logic

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 /*----- constants -----*/
 const messages = document.querySelector('h2');
-
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
 /*----- app's state (variables) -----*/
 let board;
 let turn = 'X';
+let win;
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll('#board div'));
 
@@ -31,6 +41,19 @@ function handleTurn(event) {
     });
     board[idx] = turn;
     turn = turn === 'X' ? 'O' : 'X';
+    win = board[0] && board[0] === board[1] && board[0] === board[2] ? board[0] : null;
+    win = getWinner();
     // check your console logs to make sure it's working!
     console.log(board);
+    render();
+    messages.textContent = win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+    
 };
+
+function getWinner() {
+    let winner = null;
+    winningCombos.forEach(function(combo, index) {
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+      });
+      return winner;
+    };
