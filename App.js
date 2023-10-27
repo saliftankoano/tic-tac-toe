eebe9fb018b0065eeaefbaa2aefc0dfa7f98b9f6
Salif Tankoano <tanksalif@gmail.com>
2023-10-27 17:44:51 -0400

    update game to jsx

--- App.js
+++ App.js
@@ -2,6 +2,21 @@ function App() {
     return (
       <div>
         <h1>Tic-React-Toe</h1>
+        <h2>It's X's turn!</h2>
+        <div class="flex-container flex-column">
+        <div class="flex-container flex-wrap" id="board">
+            <div class="square"></div>
+            <div class="square"></div>
+            <div class="square"></div>
+            <div class="square"></div>
+            <div class="square"></div>
+            <div class="square"></div>
+            <div class="square"></div>
+            <div class="square"></div>
+            <div class="square"></div>
+      </div>
+      <button id="reset-button">reset</button>
+    </div>
       </div>
     )
 }
